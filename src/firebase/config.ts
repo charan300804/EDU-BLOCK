e7289be3f334c0f4bd66f7dc98b24f2a9d743073
MUKTHALA SHIVA CHARAN <22e11a6646@biet.ac.in>
2025-10-19 11:19:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6552309924-c3b22",
+  "appId": "1:625216484625:web:ebe3124c423588f863c8b0",
+  "apiKey": "AIzaSyC3Xf64DtzatLjCoya_2TptfjZScKckGCg",
+  "authDomain": "studio-6552309924-c3b22.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "625216484625"
+};
